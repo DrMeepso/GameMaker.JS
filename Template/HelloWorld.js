07dfeee25071de30a530a4cbbd080ead8912c2bf
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2022-03-15 18:19:18 +1300

    Update HelloWorld.js

--- Template/HelloWorld.js
+++ Template/HelloWorld.js
@@ -5,19 +5,32 @@
 const world = new GameMaker.World(GameMaker.Init(), new GameMaker.Vecter2(0,0))
 
 //Create and add a Shape Sprite to the current world
-var MouseSprite = new GameMaker.ShapeSprite("TestObject", new GameMaker.Vecter2(1,1), new GameMaker.Vecter2(15,15))
+var MouseSprite = new GameMaker.ShapeSprite("TestObject", new GameMaker.Vecter2(1,1), new GameMaker.Vecter2(15,15), 0, '#cf4e4e')
 world.addobjects(MouseSprite)
 
+var MouseSpriteHover = new GameMaker.ShapeSprite("TestObject", new GameMaker.Vecter2(0,0), new GameMaker.Vecter2(15,15), 0, '#4ecfa8')
+world.addobjects(MouseSpriteHover)
+
 //Load the mouse plugin so we can get info about mos position
-var Mouse = new GameMaker.Plugins.Mouse("Mouse", world)
+var Mouse = new GameMaker.Plugins.Mouse(world)
+var Keybord = new GameMaker.Plugins.Keybord(world)
+
+//Create Roto var in the scope
+var Roto = 0
 
 //Update the screen
 setInterval( () => {
 
     //Set The MouseSprite's position to the position on the mouse
     MouseSprite.pos = world.plugins.mouse.pos
+    MouseSprite.angle += 1
 
+    //Make 2nd cube spin around the mouse
+    Roto += 0.2
+    MouseSpriteHover.pos.X = world.plugins.mouse.pos.X + Math.cos(Roto) * 50
+    MouseSpriteHover.pos.Y = world.plugins.mouse.pos.Y + Math.sin(Roto) * 50
+    
     //Render the world to the screen
     world.render()
     
-} ,1/60)
+} ,1000/60)
